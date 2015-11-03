d5dfa4cbb7d0edfcba2c0ff8ac09ba25c41a3701
Matheus Fernandes <matheus.souza.fernandes@gmail.com>
2015-11-03 15:55:39 -0200

    Refactoring interface, added materializecss
    Signed-off-by: Matheus Fernandes <matheus.souza.fernandes@gmail.com>

---
+++ _sass/config.rb
@@ -0,0 +1,9 @@
+require 'compass/import-once/activate'
+# Require any additional compass plugins here.
+#
+# # Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "../css"
+sass_dir = "."
+images_dir = "assets"
+javascripts_dir = "js"
