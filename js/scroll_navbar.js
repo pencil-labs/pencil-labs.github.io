90c12d9a71c315159f138942f6ff07b26fb7dba2
Matheus Fernandes <matheus.souza.fernandes@gmail.com>
2015-11-05 15:29:35 -0200

    Refactoring page
    Signed-off-by: Matheus Fernandes <matheus.souza.fernandes@gmail.com>

--- js/scroll_navbar.js
+++ js/scroll_navbar.js
@@ -1,11 +1,10 @@
-$(function(){
-  $(window).scroll(function() {
-    var bar = $('#pencil-navbar');
-    bar.fadeOut( "slow" );
-    clearTimeout($.data(this, 'scrollTimer'));
-    $.data(this, 'scrollTimer', setTimeout(function() {
-      bar.fadeIn(500).css( "display", "initial" );
-    }, 1200));
-  });
-});
+$(function() {
+    $(window).scroll(function() {
+        var bar = $('.js-navbar');
+        var section_height = $('.js-section').offset().top;
+        var top_distance = $(this).scrollTop();
+        var opacity = Math.min(top_distance / section_height, 1);
 
+        bar.stop().animate({'opacity' : opacity}, 100);
+    })
+})
